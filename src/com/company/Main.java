e005d437f9bd9c46b73dd1eee701775f2c962d98
Incicas Beniamin <incicas.beniamin@gmail.com>
2021-12-27 13:26:38 +0200

    Rent Vehicle Application merged

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Heloo");
+    public static void main(String[] args) throws CustomerExistException, VehicleNotExistException, EmptyVehiclesException {
+
+        Customer c1 = new Customer("Beniamin", " Incicas", "0767625580");
+        Vehicle v1 = new Sedan(true, false);
+
+
+        RentalCarService rentalCarService = new RentalCarService();
+        rentalCarService.addCustomer(c1);
+        rentalCarService.addVehicle(v1);
+
+
+        Order order = new Order(c1, v1, 15, 17);
+
+        rentalCarService.addOrder(order);
+        rentalCarService.rent(order);
+        rentalCarService.returnVehicle(order);
+        rentalCarService.listAllOrders();
+        System.out.println(rentalCarService);
+
+
     }
 }
