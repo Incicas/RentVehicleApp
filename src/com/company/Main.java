4765021f75e9e0a78d890363a9c4bba72d7667ff
Incicas Beniamin <incicas.beniamin@gmail.com>
2021-12-27 13:45:43 +0200

    MySQL JDBC works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,58 @@
 package com.company;
 
 
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws CustomerExistException, VehicleNotExistException, EmptyVehiclesException {
+    public static void main(String[] args) throws CustomerExistException, VehicleNotExistException, EmptyVehiclesException, SQLException {
 
-        Customer c1 = new Customer("Beniamin", " Incicas", "0767625580");
-        Vehicle v1 = new Sedan(true, false);
+//        Customer c1 = new Customer("Beniamin", " Incicas", "0767625580");
+//        Vehicle v1 = new Sedan(true, false);
+//
+//
+//        RentalCarService rentalCarService = new RentalCarService();
+//        rentalCarService.addCustomer(c1);
+//        rentalCarService.addVehicle(v1);
+//
+//
+//        Order order = new Order(c1, v1, 15, 17);
+//
+//        rentalCarService.addOrder(order);
+//        rentalCarService.rent(order);
+//        rentalCarService.returnVehicle(order);
+//        rentalCarService.listAllOrders();
+//        System.out.println(rentalCarService);
 
+        System.out.println(" Operations you can do on database: ");
+        System.out.println("1. Create ");
+        System.out.println("2. Read ");
+        System.out.println("3. Update ");
+        System.out.println("4. Delete ");
+        System.out.println("Your option is: ");
 
-        RentalCarService rentalCarService = new RentalCarService();
-        rentalCarService.addCustomer(c1);
-        rentalCarService.addVehicle(v1);
+        Scanner sc = new Scanner(System.in);
+        int choise = sc.nextInt();
+        MySqlAccess read = new MySqlAccess();
+        // switch statement with int data type
+        switch (choise) {
 
+            case 1:
+                read.create();
+                break;
+            case 2:
+                read.read();
+                break;
+            case 3:
+                read.update();
+                break;
+            case 4:
+                read.delete();
+                break;
 
-        Order order = new Order(c1, v1, 15, 17);
-
-        rentalCarService.addOrder(order);
-        rentalCarService.rent(order);
-        rentalCarService.returnVehicle(order);
-        rentalCarService.listAllOrders();
-        System.out.println(rentalCarService);
-
-
+            default:
+                System.out.println("Invalid method");
+        }
     }
 }
